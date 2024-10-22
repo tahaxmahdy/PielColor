ba138cceb091ca6b188f6414f9524e633b44c67e
tahaxmahdy <tahamahdy107@gmail.com>
2024-10-22 11:21:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("toggleDrawer").addEventListener("click", function () {
+	const drawer = document.getElementById("drawer");
+	drawer.classList.toggle("active"); // إضافة أو إزالة الكلاس active
+});
